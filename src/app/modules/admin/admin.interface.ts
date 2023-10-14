5306ac42a6de3d6c851f6f0d577cd0d4bd4fba6e
M Rafiul Alam Tonmoy <mrafiul.alam7@gmail.com>
2023-10-14 09:22:01 +0600

    ADDED: Admin Interface

--- src/app/modules/admin/admin.interface.ts
+++ src/app/modules/admin/admin.interface.ts
@@ -2,10 +2,15 @@ import { Model } from 'mongoose'
 import { UserName } from '../../../interface/username'
 
 export type IAdmin = {
+  email: string
   name: UserName
-  role: 'admin'
+  phoneNo: string
   phoneNumber: string
+  password: string
   address: string
+  role: 'admin'
+  profileImg: string
+  permissions?: ['view', 'update', 'delete']
 }
 
 export type AdminModel = Model<IAdmin>
